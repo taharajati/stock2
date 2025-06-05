b4ab439153684a93707db8128ce949f233a3b69d
taharajati <taharjt.developer@gmail.com>
2025-06-05 22:25:25 +0330

    02

--- server.js
+++ server.js
@@ -90,7 +90,7 @@ app.get('/auth/logout', (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Backend server is running on port ${PORT}`);
 }); 
